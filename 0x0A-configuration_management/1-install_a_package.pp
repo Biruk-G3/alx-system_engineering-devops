1fbd4220fffc285c4039dd4f4d19ad6c14cdf67c
BirukG <byafet3@gmail.com>
2022-10-14 10:20:20 -0700

    fixed

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # install flask -v 2.1.0
 
-exec { 'install python packages':
-     command   => 'pip3 install flask flask_restful apiai -v 2.1.0',
-     path => ['/usr/bin/'],
-     unless  => '/usr/bin/test -f /usr/local/lib/python3.4/dist-packages/flask/app.py'
-  }
+exec { 'flask':
+  command => '/usr/bin/apt-get -y pip install flask -v 2.5.0',
+}
